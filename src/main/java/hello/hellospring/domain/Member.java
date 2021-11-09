a2518b2c9c6d555ded8f98d87165fb9dedc43aab
rissins <tnalsqqq@naver.com>
2021-11-10 00:40:00 +0900

    initial commit

---
+++ src/main/java/hello/hellospring/domain/Member.java
@@ -0,0 +1,30 @@
+package hello.hellospring.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Member {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) // IDENTITY : db가 자동으로 알아서  생성해주는것
+    private Long id;
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
